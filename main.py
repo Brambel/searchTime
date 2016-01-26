74a9d16f0928d7a7982f8a4fd21ce8e03f05fa54
Brambel <jamesebert1@gmail.com>
2016-01-26 08:23:23 -0600

    added legend and multiple tests
    we do each tier 20 times and take the avg time to remove extremesfrom
    the graph

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import matplotlib.pyplot as plt
 import random
 import time
-import math
+import matplotlib.patches as patch
 
-size = [5,10,50,100,500,1000,5000,10000]
+size = [5,10,50,100,500,1000]
 sortable = []
 points = []
 logs = []
+tempTimes = []
 for n in range(size.__len__()):
     t1 = time.time()
-    for x in range(size[n]):
-        sortable.append(random.seed())
-    sortable.sort()
-    t2 = time.time()
+    for x in range(20):
+        for x in range(size[n]):
+            sortable.append(random.seed())
+        sortable.sort()
+        t2 = time.time()
+        tempTimes.append(t2-t1)
     logs.append(n*n)
-    points.append(t2-t1)
-
-
+    points.append(sum(tempTimes)/20)
+redPatch = patch.Patch(color='red', label='Sort Time')
+bluePatch = patch.Patch(color='blue', label='n^2')
+plt.legend(handles=[redPatch, bluePatch], loc=2)
 plt.plot(points, 'r', logs, 'b')
 plt.ylabel('some num')
 plt.show()
