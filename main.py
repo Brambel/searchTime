c945ff0e6ec985e94d9e3f3849153e23059b7a4e
Brambel <jamesebert1@gmail.com>
2016-01-26 09:25:16 -0600

    final
    fixed timing error

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ import matplotlib.pyplot as plt
 import random
 import time
 import matplotlib.patches as patch
+import math
 
-size = [5,10,50,100,500,1000]
+size = [5,10,50,100,500,1000,5000,10000]
 sortable = []
 points = []
 logs = []
 tempTimes = []
-for n in range(size.__len__()):
-    t1 = time.time()
-    for x in range(20):
-        for x in range(size[n]):
+for n in range(1,size.__len__()+1):
+    for x in range(10):
+        t1 = time.time()
+        for x in range(size[n-1]):
             sortable.append(random.seed())
         sortable.sort()
         t2 = time.time()
         tempTimes.append(t2-t1)
-    logs.append(n*n)
+    logs.append(n*math.log(n,2))
     points.append(sum(tempTimes)/20)
+
 redPatch = patch.Patch(color='red', label='Sort Time')
-bluePatch = patch.Patch(color='blue', label='n^2')
+bluePatch = patch.Patch(color='blue', label='n(lg(n))')
 plt.legend(handles=[redPatch, bluePatch], loc=2)
 plt.plot(points, 'r', logs, 'b')
-plt.ylabel('some num')
+plt.ylabel('time in seconds')
 plt.show()
